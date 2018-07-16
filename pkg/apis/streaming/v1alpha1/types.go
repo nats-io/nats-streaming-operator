7e33478be69b673eea104c3e8f59f0d635b9aded
Waldemar Quevedo <wally@synadia.com>
2018-07-16 10:34:18 -0700

    Update types, add boilerplate
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- pkg/apis/streaming/v1alpha1/types.go
+++ pkg/apis/streaming/v1alpha1/types.go
@@ -4,16 +4,20 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// NatsStreamingClusterList
+// 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
 type NatsStreamingClusterList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 	Items           []NatsStreamingCluster `json:"items"`
 }
 
+// NatsStreamingCluster
+// 
+// +genclient
+// +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
 type NatsStreamingCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata"`
@@ -25,8 +29,12 @@ type NatsStreamingClusterSpec struct {
 	// Size is the number of nodes in the NATS Streaming cluster.
 	Size int32 `json:"size"`
 
-	// NatsService is the Kubernetes service to which the
-	// NATS Streaming nodes will connect.
+	// Version is the version of NATS Streaming that is being used.
+	// By default it will be the latest version.
+	NatsStreamingImage string `json:"image"`
+
+	// NatsService is the Kubernetes service to which the NATS Streaming nodes will connect.
+	// The service has to be in the same namespace as the NATS Operator.
 	NatsService string `json:"natsSvc"`
 }
 
