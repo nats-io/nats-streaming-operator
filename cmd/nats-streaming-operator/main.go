d1849f9433f8dc6cbd19b6eb5c058c47d3203020
Waldemar Quevedo <wally@synadia.com>
2018-07-25 01:25:58 -0700

    Allow debug level for logging
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- cmd/nats-streaming-operator/main.go
+++ cmd/nats-streaming-operator/main.go
@@ -2,40 +2,42 @@ package main
 
 import (
 	"context"
+	"os"
 	"runtime"
 
 	stub "github.com/nats-io/nats-streaming-operator/pkg/stub"
 	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
 	k8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
-
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
-func printVersion() {
-	logrus.Infof("Go Version: %s", runtime.Version())
-	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
-	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
-}
-
 func main() {
-	printVersion()
+	if os.Getenv("DEBUG") == "true" {
+		log.SetLevel(log.DebugLevel)
+	}
+	formatter := &log.TextFormatter{
+		FullTimestamp: true,
+	}
+	log.SetFormatter(formatter)
 
-	sdk.ExposeMetricsPort()
+	log.Infof("Starting NATS Streaming Operator v%s", "0.1.0")
+	log.Infof("Go Version: %s", runtime.Version())
+	log.Infof("operator-sdk Version: %v", sdkVersion.Version)
 
 	resource := "streaming.nats.io/v1alpha1"
 	kind := "NatsStreamingCluster"
 	namespace, err := k8sutil.GetWatchNamespace()
 	if err != nil {
-		logrus.Fatalf("Failed to get watch namespace: %v", err)
+		log.Fatalf("Failed to get watch namespace: %v", err)
 	}
+
+	// TODO: Move to constants
 	resyncPeriod := 5
-	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
+	log.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
 
 	// Look for updates on the NatsStreamingClusters made on this namespace.
 	sdk.Watch(resource, kind, namespace, resyncPeriod)
-
-	// 
 	sdk.Handle(stub.NewHandler())
 	sdk.Run(context.Background())
 }
