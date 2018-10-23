3f8a82c9779e448293d1ac17d7d21ec8c3364524
Michael <MichaelSp@users.noreply.github.com>
2018-10-23 11:51:54 +0200

    update streaming server to v0.11.2

--- internal/operator/constants.go
+++ internal/operator/constants.go
@@ -17,7 +17,7 @@ const (
 	// DefaultNATSStreamingImage is the default image
 	// of NATS Streaming that will be used, meant to be
 	// the latest release available.
-	DefaultNATSStreamingImage = "nats-streaming:0.10.2"
+	DefaultNATSStreamingImage = "nats-streaming:0.11.2"
 
 	// DefaultNATSStreamingClusterSize is the default size
 	// for the cluster.  Clustering is done via Raft so
