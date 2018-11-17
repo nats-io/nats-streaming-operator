fb57c30966f95dee4109927b078b5781f1b8731f
Waldemar Quevedo <wally@synadia.com>
2018-11-17 14:09:15 -0800

    Add store, config file and Pod spec template to types
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- pkg/apis/streaming/v1alpha1/register.go
+++ pkg/apis/streaming/v1alpha1/register.go
@@ -14,8 +14,6 @@
 package v1alpha1
 
 import (
-	sdkK8sutil "github.com/operator-framework/operator-sdk/pkg/util/k8sutil"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -32,10 +30,6 @@ var (
 	// SchemeGroupVersion is the group version used to register these objects.
 	SchemeGroupVersion = schema.GroupVersion{Group: groupName, Version: version}
 )
-
-func init() {
-	sdkK8sutil.AddToSDKScheme(AddToScheme)
-}
 
 // addKnownTypes adds the set of types defined in this package to the supplied scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
